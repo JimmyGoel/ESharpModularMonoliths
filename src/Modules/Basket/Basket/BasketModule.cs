06001881a9977877aaae2a02808a1074a5468a24
JimmyGoel <goel.jimmy@gmail.com>
2025-10-06 12:19:15 +0530

    Built Structure

---
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket
+{
+    public static class BasketModule
+    {
+        public static IServiceCollection AddBasketModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add Catalog module services here
+            return services;
+        }
+    }
+}
