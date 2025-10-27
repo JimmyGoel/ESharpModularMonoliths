9dc9115a07ffb9fa831416c7c9eb7b9116e6307d
JimmyGoel <goel.jimmy@gmail.com>
2025-10-27 17:43:54 +0530

    Implement CQRS with Mediater pattern

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct
+{
+    public record CreateProductCommand(
+    string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : IRequest<CreateProudctResult>;
+    public record CreateProudctResult(Guid ProductId);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProudctResult>
+    {
+        public Task<CreateProudctResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Shared/Shared/CQRS/ICommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.CQRS
+{
+    public interface ICommandHandler<in TCommand>
+        : ICommandHandler<TCommand, Unit>
+        where TCommand : ICommand<Unit>
+    { }
+    public interface ICommandHandler<in TCommand, TResonse>
+        : IRequestHandler<TCommand, TResonse>
+        where TCommand : ICommand<TResonse>
+        where TResonse : notnull
+    { }
+}
