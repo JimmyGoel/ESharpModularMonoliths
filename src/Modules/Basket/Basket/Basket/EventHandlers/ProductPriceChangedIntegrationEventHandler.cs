2c48f34879c368e5fdb1a178e75f4ca728c07349
JimmyGoel <goel.jimmy@gmail.com>
2025-11-26 17:36:18 +0530

    changes

--- src/Modules/Basket/Basket/Basket/EventHandlers/ProductPriceChangedIntegrationEventHandler.cs
+++ src/Modules/Basket/Basket/Basket/EventHandlers/ProductPriceChangedIntegrationEventHandler.cs
@@ -1,4 +1,5 @@
-﻿using MassTransit;
+﻿using Basket.Basket.Features.UpdateItemPriceBasket;
+using MassTransit;
 using Microsoft.Extensions.Logging;
 using Shared.Messaging.Events;
 
@@ -8,10 +9,22 @@ namespace Basket.Basket.EventHandlers
         (ISender sender, ILogger<ProductPriceChangedIntegrationEventHandler> logger) :
         IConsumer<ProductPriceChangeIntegrationEvent>
     {
-        public Task Consume(ConsumeContext<ProductPriceChangeIntegrationEvent> context)
+        public async Task Consume(ConsumeContext<ProductPriceChangeIntegrationEvent> context)
         {
             logger.LogInformation("Integration Event handler : {IntegrationEvent}", context.Message.GetType().Name);
-            return Task.CompletedTask;
+
+            var command = new UpdateItemPriceInBasketComand(NewPrice: context.Message.Price, ProductId: context.Message.ProductId);
+            var result = await sender.Send(command);
+
+            if (!result.IsPriceUpdated)
+            {
+                logger.LogInformation("Basket items updated for ProductId: {ProductId} with New Price: {NewPrice}", context.Message.ProductId, context.Message.Price);
+            }
+            else
+            {
+                logger.LogWarning("No basket items found for ProductId: {ProductId} to update price.", context.Message.ProductId);
+            }
+
         }
     }
 }
